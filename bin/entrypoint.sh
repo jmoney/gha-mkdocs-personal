edc3052d9d9a11285152538e0de9277e7a030bc7
Jonathan Monette <jon.monette@gmail.com>
2023-08-08 09:55:31 -0500

    Overwrite index.md and license.md instead of including content from the parent

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-mkdir ./docs/img
+mkdir -p ./docs
+mkdir -p ./docs/img
 cp /app/mkdocs.yml .
 cp /app/favicon.ico ./docs/img
 cp --recursive /app/hooks .
+cp README.md ./docs/index.md
+cp LICESNE ./docs/license.md
+
 mkdocs build --clean
