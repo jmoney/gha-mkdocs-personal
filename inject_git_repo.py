f5cfc128b7678a4c06327f5f4564957a45c472c0
Jonathan Monette <jon.monette@gmail.com>
2023-08-08 08:41:56 -0500

    add simple hook

--- inject_git_repo.py
+++ inject_git_repo.py
@@ -1,13 +1,12 @@
 import os
 import ghapi.all as ghapi
-from mkdocs.config import base, config_options as c
+import mkdocs.plugins
 
 GITHUB_REPOSITORY = os.environ.get('GITHUB_REPOSITORY')
 
-class InjectGitRepoPlugin(base.BasePlugin):
-        
-        def on_config(self, config, **kwargs):
-            config['repo_name'] = GITHUB_REPOSITORY
-            config['repo_url'] = f"https://gitbug.com/{GITHUB_REPOSITORY}"
-            config.theme.icon.repo = "simple/github"
-            return config
+@mkdocs.plugins.event_priority(-50)        
+def on_config(config, **kwargs):
+    config['repo_name'] = GITHUB_REPOSITORY
+    config['repo_url'] = f"https://gitbug.com/{GITHUB_REPOSITORY}"
+    config.theme.icon.repo = "simple/github"
+    return config
