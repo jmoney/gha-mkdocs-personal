6b5a4e91a434b0ad02ff449ccfae5de6cfe898c1
Jonathan Monette <jon.monette@gmail.com>
2023-08-08 20:00:50 -0500

    fix urls

--- hooks/config/inject_git_repo.py
+++ hooks/config/inject_git_repo.py
@@ -7,5 +7,7 @@ GITHUB_REPOSITORY = os.environ.get('GITHUB_REPOSITORY')
 @mkdocs.plugins.event_priority(-50)        
 def on_config(config, **kwargs):
     config['repo_name'] = GITHUB_REPOSITORY
-    config['repo_url'] = f"https://gitbug.com/{GITHUB_REPOSITORY}"
+    config['repo_url'] = f"https://github.com/{GITHUB_REPOSITORY}"
+    config['repo_owner'] = GITHUB_REPOSITORY.split('/')[0]
+    config['repo'] = GITHUB_REPOSITORY.split('/')[1]
     return config
