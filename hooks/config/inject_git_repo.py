dc26aba19f3e27ac79ce15ee974a1fe52f9c0a03
Jonathan Monette <jon.monette@gmail.com>
2023-08-08 21:34:06 -0500

    add some logging

--- hooks/config/inject_git_repo.py
+++ hooks/config/inject_git_repo.py
@@ -1,13 +1,14 @@
+import logging
 import os
 import ghapi.all as ghapi
 import mkdocs.plugins
 
 GITHUB_REPOSITORY = os.environ.get('GITHUB_REPOSITORY')
+log = logging.getLogger('mkdocs')
 
 @mkdocs.plugins.event_priority(-50)        
 def on_config(config, **kwargs):
+    log.info(f"Injecting git repo info into config: {GITHUB_REPOSITORY}")
     config['repo_name'] = GITHUB_REPOSITORY
     config['repo_url'] = f"https://github.com/{GITHUB_REPOSITORY}"
-    config['repo_owner'] = GITHUB_REPOSITORY.split('/')[0]
-    config['repo'] = GITHUB_REPOSITORY.split('/')[1]
     return config
